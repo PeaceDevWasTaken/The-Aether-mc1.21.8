7faf22a3e41595aecabd8284ea26c6ede83918fb
bconlon <bconlon.gml@gmail.com>
2025-01-15 16:22:52 -0800

    fix: fluid type check

--- src/main/java/com/aetherteam/aether/utils/FabricUtils.java
+++ src/main/java/com/aetherteam/aether/utils/FabricUtils.java
@@ -2,18 +2,22 @@ package com.aetherteam.aether.utils;
 
 import com.aetherteam.aether.mixin.mixins.common.accessor.EntityAccessor;
 import io.github.fabricators_of_create.porting_lib.enchant.CustomEnchantingBehaviorItem;
+import it.unimi.dsi.fastutil.objects.Object2DoubleMap;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.material.Fluid;
 
 import java.util.Map;
 
 public class FabricUtils {
-
     public static boolean isInFluidType(Entity livingEntity) {
-        return ((EntityAccessor) livingEntity).getFluidHeight().size() > 0;
+        for (Object2DoubleMap.Entry<TagKey<Fluid>> entry : ((EntityAccessor) livingEntity).getFluidHeight().object2DoubleEntrySet()) {
+            return entry.getDoubleValue() > 0;
+        }
+        return false;
     }
 
     public static boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
