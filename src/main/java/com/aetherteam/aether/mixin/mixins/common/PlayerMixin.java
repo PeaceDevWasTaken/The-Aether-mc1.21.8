e8a771ffaf75902e3a2b653935696d21cb1de66a
bconlon <bconlon.gml@gmail.com>
2025-01-22 19:43:03 -0800

    fix: sword abilities

--- src/main/java/com/aetherteam/aether/mixin/mixins/common/PlayerMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/common/PlayerMixin.java
@@ -2,15 +2,31 @@ package com.aetherteam.aether.mixin.mixins.common;
 
 import com.aetherteam.aether.entity.passive.MountableAnimal;
 import com.aetherteam.aether.event.hooks.AbilityHooks;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
-public class PlayerMixin {
+public abstract class PlayerMixin {
+    @Shadow
+    public abstract void resetAttackStrengthTicker();
+
+    @WrapOperation(method = "attack(Lnet/minecraft/world/entity/Entity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;resetAttackStrengthTicker()V"))
+    private void aetherFabric$preventResetHere(Player instance, Operation<Void> original) {
+        // NO-OP
+    }
+
+    @Inject(method = "attack(Lnet/minecraft/world/entity/Entity;)V", at = @At(value = "RETURN", ordinal = 2))
+    private void aetherFabric$callResetAtEnd(Entity target, CallbackInfo ci) {
+        this.resetAttackStrengthTicker();
+    }
+
     /**
      * Damages gloves only once during a sweeping attack, instead of once for every damaged entity in the attack.
      * @param target The target {@link Entity}.
