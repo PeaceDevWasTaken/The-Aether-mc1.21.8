97c5d7c7086218813513349e8d2b906adf6b4fac
bconlon <bconlon.gml@gmail.com>
2025-01-27 11:01:32 -0800

    fix: accessory inventory hotkey no longer overrides mount inventory

--- src/main/java/com/aetherteam/aether/client/event/hooks/GuiHooks.java
+++ src/main/java/com/aetherteam/aether/client/event/hooks/GuiHooks.java
@@ -262,8 +262,12 @@ public class GuiHooks {
         Minecraft minecraft = Minecraft.getInstance();
         if (minecraft.player != null && minecraft.getOverlay() == null && minecraft.screen == null) {
             if (!AetherConfig.CLIENT.disable_accessory_button.get() && AetherKeys.OPEN_ACCESSORY_INVENTORY.consumeClick()) {
-                PacketDistributor.sendToServer(new OpenAccessoriesPacket(ItemStack.EMPTY));
-                shouldAddButton = false; // The AccessoryButton is not added to menus opened with the key.
+                if (minecraft.gameMode != null && minecraft.gameMode.isServerControlledInventory()) {
+                    minecraft.player.sendOpenInventory();
+                } else {
+                    PacketDistributor.sendToServer(new OpenAccessoriesPacket(ItemStack.EMPTY));
+                    shouldAddButton = false; // The AccessoryButton is not added to menus opened with the key.
+                }
             }
         }
     }
