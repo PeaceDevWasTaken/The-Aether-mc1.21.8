73a561c256f42c5a32098d814a4ecc0fe0959b84
bconlon <bconlon.gml@gmail.com>
2025-02-19 13:24:24 -0800

    fix: merge error

--- src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/goal/SliderNearestAttackableTargetGoal.java
+++ src/main/java/com/aetherteam/aether/entity/monster/dungeon/boss/goal/SliderNearestAttackableTargetGoal.java
@@ -4,8 +4,8 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.function.Predicate;
 
 public class SliderNearestAttackableTargetGoal<T extends LivingEntity> extends NearestAttackableTargetGoal<T> {
