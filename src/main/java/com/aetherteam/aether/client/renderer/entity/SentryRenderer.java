1d694123179ec83ac45467df5f10d01fccb4272f
baguchi <88538210+baguchi@users.noreply.github.com>
2025-05-04 03:33:51 +0900

    feat: 1.21.3 Port with Render Fix (#2652)
    This pr main to fix entity rendering such as EntityRenderState Stuff
    also support AgeableMobRenderer

--- src/main/java/com/aetherteam/aether/client/renderer/entity/SentryRenderer.java
+++ src/main/java/com/aetherteam/aether/client/renderer/entity/SentryRenderer.java
@@ -3,6 +3,7 @@ package com.aetherteam.aether.client.renderer.entity;
 import com.aetherteam.aether.Aether;
 import com.aetherteam.aether.client.renderer.AetherModelLayers;
 import com.aetherteam.aether.client.renderer.entity.layers.SentryGlowLayer;
+import com.aetherteam.aether.client.renderer.entity.state.SentryRenderState;
 import com.aetherteam.aether.entity.monster.dungeon.Sentry;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.model.SlimeModel;
@@ -10,13 +11,24 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
-public class SentryRenderer extends MobRenderer<Sentry, SlimeModel<Sentry>> {
+public class SentryRenderer extends MobRenderer<Sentry, SentryRenderState, SlimeModel> {
     private static final ResourceLocation SENTRY_TEXTURE = ResourceLocation.fromNamespaceAndPath(Aether.MODID, "textures/entity/mobs/sentry/sentry.png");
     private static final ResourceLocation SENTRY_LIT_TEXTURE = ResourceLocation.fromNamespaceAndPath(Aether.MODID, "textures/entity/mobs/sentry/sentry_lit.png");
 
     public SentryRenderer(EntityRendererProvider.Context context) {
-        super(context, new SlimeModel<>(context.bakeLayer(AetherModelLayers.SENTRY)), 0.3F);
+        super(context, new SlimeModel(context.bakeLayer(AetherModelLayers.SENTRY)), 0.3F);
         this.addLayer(new SentryGlowLayer(this));
+    }
+
+    @Override
+    public SentryRenderState createRenderState() {
+        return new SentryRenderState();
+    }
+
+    @Override
+    public void extractRenderState(Sentry entity, SentryRenderState reusedState, float partialTick) {
+        super.extractRenderState(entity, reusedState, partialTick);
+        reusedState.awake = entity.isAwake();
     }
 
     /**
@@ -24,20 +36,19 @@ public class SentryRenderer extends MobRenderer<Sentry, SlimeModel<Sentry>> {
      *
      * @param sentry       The {@link Sentry} entity.
      * @param poseStack    The rendering {@link PoseStack}.
-     * @param partialTicks The {@link Float} for the game's partial ticks.
      */
     @Override
-    protected void scale(Sentry sentry, PoseStack poseStack, float partialTicks) {
+    protected void scale(SentryRenderState sentry, PoseStack poseStack) {
         float f = 0.879F;
         poseStack.scale(f, f, f);
-        float f1 = sentry.getSize() + 1.0F;
+        float f1 = sentry.scale + 1.0F;
         float f2 = 0.0F;
         float f3 = 1.0F / (f2 + 1.0F);
         poseStack.scale(f3 * f1, 1.0F / f3 * f1, f3 * f1);
     }
 
     @Override
-    public ResourceLocation getTextureLocation(Sentry sentry) {
-        return sentry.isAwake() ? SENTRY_LIT_TEXTURE : SENTRY_TEXTURE;
+    public ResourceLocation getTextureLocation(SentryRenderState sentry) {
+        return sentry.awake ? SENTRY_LIT_TEXTURE : SENTRY_TEXTURE;
     }
 }
--- src/main/java/com/aetherteam/aether/client/renderer/entity/layers/SentryGlowLayer.java
+++ src/main/java/com/aetherteam/aether/client/renderer/entity/layers/SentryGlowLayer.java
@@ -1,21 +1,23 @@
 package com.aetherteam.aether.client.renderer.entity.layers;
 
 import com.aetherteam.aether.Aether;
+import com.aetherteam.aether.client.renderer.entity.state.SentryRenderState;
 import com.aetherteam.aether.entity.monster.dungeon.Sentry;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.model.SlimeModel;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.EyesLayer;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 
-public class SentryGlowLayer extends EyesLayer<Sentry, SlimeModel<Sentry>> {
+public class SentryGlowLayer<T extends EntityRenderState> extends EyesLayer<T, EntityModel<T>> {
     private static final RenderType SENTRY_EYE = RenderType.eyes(ResourceLocation.fromNamespaceAndPath(Aether.MODID, "textures/entity/mobs/sentry/eye.png"));
 
-    public SentryGlowLayer(RenderLayerParent<Sentry, SlimeModel<Sentry>> entityRenderer) {
+    public SentryGlowLayer(RenderLayerParent<T, EntityModel<T>> entityRenderer) {
         super(entityRenderer);
     }
 
@@ -26,17 +28,13 @@ public class SentryGlowLayer extends EyesLayer<Sentry, SlimeModel<Sentry>> {
      * @param buffer          The rendering {@link MultiBufferSource}.
      * @param packedLight     The {@link Integer} for the packed lighting for rendering.
      * @param sentry          The {@link Sentry} entity.
-     * @param limbSwing       The {@link Float} for the limb swing rotation.
-     * @param limbSwingAmount The {@link Float} for the limb swing amount.
-     * @param partialTicks    The {@link Float} for the game's partial ticks.
-     * @param ageInTicks      The {@link Float} for the entity's age in ticks.
      * @param netHeadYaw      The {@link Float} for the head yaw rotation.
      * @param headPitch       The {@link Float} for the head pitch rotation.
      */
     @Override
-    public void render(PoseStack poseStack, MultiBufferSource buffer, int packedLight, Sentry sentry, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+    public void render(PoseStack poseStack, MultiBufferSource buffer, int packedLight, T sentry, float netHeadYaw, float headPitch) {
         VertexConsumer consumer = buffer.getBuffer(this.renderType());
-        if (sentry.isAwake()) {
+        if (sentry instanceof SentryRenderState state && state.awake) {
             this.getParentModel().renderToBuffer(poseStack, consumer, 15728640, OverlayTexture.NO_OVERLAY);
         }
     }
