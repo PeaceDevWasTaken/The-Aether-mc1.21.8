2c6969e3afd852ef449188f0d384b0333dee486f
bconlon <bconlon.gml@gmail.com>
2025-08-06 09:04:34 -0700

    improv: bucket capability (#2568)

---
+++ src/main/java/com/aetherteam/aether/item/miscellaneous/bucket/SkyrootBucketWrapper.java
@@ -0,0 +1,28 @@
+package com.aetherteam.aether.item.miscellaneous.bucket;
+
+import com.aetherteam.aether.AetherTags;
+import com.aetherteam.aether.item.AetherItems;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper;
+
+public class SkyrootBucketWrapper extends FluidBucketWrapper {
+    public SkyrootBucketWrapper(ItemStack container) {
+        super(container);
+    }
+
+    @Override
+    public boolean canFillFluidType(FluidStack fluid) {
+        return fluid.getFluid().is(AetherTags.Fluids.ALLOWED_BUCKET_PICKUP);
+    }
+
+    @Override
+    protected void setFluid(FluidStack fluidStack) {
+        if (fluidStack.isEmpty()) {
+            this.container = new ItemStack(AetherItems.SKYROOT_BUCKET.get());
+        } else {
+            this.container = SkyrootBucketItem.swapBucketType(FluidUtil.getFilledBucket(fluidStack));
+        }
+    }
+}
